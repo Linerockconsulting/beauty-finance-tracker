461fa505e01d28e3faebb3e2388e3bfd45468b8f
Linerockconsulting <linerockconsulting@gmail.com>
2025-07-25 18:55:26 +0530

    Update beauty_finance_tracke.py

--- beauty_finance_tracke.py
+++ beauty_finance_tracke.py
@@ -43,8 +43,7 @@ def add_expense(date, exp_type, amount):
 # --- Menu ---
 menu = st.sidebar.radio("Go to", ["📊 Dashboard", "➕ Add Entry", "📈 View Report"])
 income_data, expense_data = load_data()
-st.write("Raw income data:", income_data)
-st.write("Raw expense data:", expense_data)
+
 
 
 
